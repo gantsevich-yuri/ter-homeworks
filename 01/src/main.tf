94342e83fc82bcaa61de075bc55a68ca7b8af4f2
foxkids <foxkids@avx.local>
2026-06-28 16:16:06 +0300

    task 1

--- 01/src/main.tf
+++ 01/src/main.tf
@@ -1,15 +1,12 @@
 terraform {
   required_providers {
     docker = {
-      source  = "kreuzwerker/docker"
+      source = "kreuzwerker/docker"
     }
   }
-  required_version = "~>1.12.0" /*Многострочный комментарий.
- Требуемая версия terraform */
+  required_version = "~>1.15.0"
 }
 provider "docker" {}
-
-#однострочный комментарий
 
 resource "random_password" "random_string" {
   length      = 16
@@ -19,19 +16,17 @@ resource "random_password" "random_string" {
   min_numeric = 1
 }
 
-/*
-resource "docker_image" {
+resource "docker_image" "nginx" {
   name         = "nginx:latest"
   keep_locally = true
 }
 
-resource "docker_container" "1nginx" {
+resource "docker_container" "nginx" {
   image = docker_image.nginx.image_id
-  name  = "example_${random_password.random_string_FAKE.resulT}"
+  name  = "example_${random_password.random_string.result}"
 
   ports {
     internal = 80
     external = 9090
   }
 }
-*/
