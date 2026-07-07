e39566ebc3bd74c68e13c1e4677c0592bd444ee6
foxkids <foxkids@avx.local>
2026-07-07 21:35:25 +0300

    task2

--- 02/src/variables.tf
+++ 02/src/variables.tf
@@ -28,6 +28,35 @@ variable "vpc_name" {
   description = "VPC network & subnet name"
 }
 
+variable "vm_web_family" {
+  type    = string
+  default = "ubuntu-2004-lts"
+}
+
+variable "vm_web_name" {
+  type    = string
+  default = "netology-develop-platform-web"
+}
+
+variable "vm_web_platform_id" {
+  type    = string
+  default = "standard-v3"
+}
+
+variable "vm_web_cores" {
+  type    = number
+  default = 2
+}
+
+variable "vm_web_memory" {
+  type    = number
+  default = 1
+}
+
+variable "vm_web_core_fraction" {
+  type    = number
+  default = 20
+}
 
 ###ssh vars
 
